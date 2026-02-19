5f5b18db4a29da079ff422318cf2bf042ea3cd40
masumislambadsha <nkfhhdndjdh@gmail.com>
2026-02-20 00:10:22 +0600

    updated hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,17 +4,20 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <section className=" px-4 py-6 lg:px-8">
-
       <div className="max-w-[1320px] mx-auto">
-        <div className="font-bold text-[209.5px]  uppercase text">
-            <h1 className="font-black text-center">
-              <span className="text-[#232321]">DO IT </span>
-              <span className="text-[#4A69E2]">RIGHT</span>
-            </h1>
-          </div>
+        <div className="font-bold uppercase text">
+          <h1 className="font-black text-[209.5px] leading-[0.8] tracking-normal uppercase text-center">
+            <span className="text-[#232321] inline-block translate-y-[6px]">
+              DO IT
+            </span>{" "}
+            <span className="text-[#4A69E2] inline-block translate-y-[6px]">
+              RIGHT
+            </span>
+          </h1>
+        </div>
 
         {/* Hero Card with Background Image and Title Inside */}
-        <div className="relative rounded-3xl overflow-hidden shadow-2xl h-[600px] lg:h-[700px]">
+        <div className="relative mt-10 rounded-3xl overflow-hidden shadow-2xl h-[600px] lg:h-[700px]">
           {/* Background Image - hero-main.png */}
           <Image
             src="/hero-main.png"
@@ -24,7 +27,7 @@ export default function Hero() {
             priority
           />
           {/* Left Badge */}
-          <div className="absolute left-0 top-40 -translate-y-1/2 bg-[#FF6B35] text-white px-2 py-6 rounded-r-md z-20">
+          <div className="absolute left-0 top-40 -translate-y-1/2 bg-[#232321] font-semibold text-[#E7E7E3] p-4 rounded-r-md z-20">
             <p className="text-[10px] font-medium [writing-mode:vertical-lr] rotate-180 whitespace-nowrap">
               Nike product of the year
             </p>
@@ -36,7 +39,7 @@ export default function Hero() {
               NIKE AIR MAX
             </h2>
             <p className="text-white text-sm lg:text-base mb-4 max-w-xs">
-              Nike introducing the new air max for everyone's comfort
+              Nike introducing the new air max for {"everyone's"} comfort
             </p>
             <Link
               href="/products"
